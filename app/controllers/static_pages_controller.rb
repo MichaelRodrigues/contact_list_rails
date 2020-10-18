f625611d2f2b91ef1b266a3e496f7320772c22b3
Micahel Rodrigues <michael.brendo.14@gmail.com>
2020-10-18 15:46:08 -0300

    Adicionado as páginas estáticas e seus caminhos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,12 @@ class StaticPagesController < ApplicationController
     def index
         @nome = "Michael"
     end
+
+    def sobre
+        
+    end
+
+    def contato
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   # Parâmetro (to:) recebe a rota com o nome do Controller (static_pages) e o nome da Action (index)
   root to: "static_pages#index"
+
+  get "sobre", to: "static_pages#sobre"
+  get "contato", to: "static_pages#contato"
 end
