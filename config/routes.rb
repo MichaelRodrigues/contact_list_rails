368beac1c3898424009ec2ba98d44b86544bd4d7
Micahel Rodrigues <michael.brendo.14@gmail.com>
2020-10-18 16:09:18 -0300

    Adicionado um crude para gerenciar o nome e telefone

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   # Parâmetro (to:) recebe a rota com o nome do Controller (static_pages) e o nome da Action (index)
