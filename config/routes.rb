0abd303a784f5d3812a8f3de04a21a3fa3a96163
Micahel Rodrigues <michael.brendo.14@gmail.com>
2020-10-18 16:55:09 -0300

    Criada a validação inicial do cadastro de usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   # Parâmetro (to:) recebe a rota com o nome do Controller (static_pages) e o nome da Action (index)
   root to: "static_pages#index"
-
+  
   get "sobre", to: "static_pages#sobre"
   get "contato", to: "static_pages#contato"
+  
+  resources :contacts
+  resources :users, only: [:new, :create]
 end
