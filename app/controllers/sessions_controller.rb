b17269d16fa39d075df36542d288c534a0d86e4f
Micahel Rodrigues <michael.brendo.14@gmail.com>
2020-10-19 12:42:18 -0300

    Configurado o relacionamento entre usuário e contato

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       sign_in(user)
-      redirect_to user_path(user)
+      redirect_to contacts_path
     else
       flash.now[:danger] = "E-mail ou Senha inválidos!"
       render "now"
